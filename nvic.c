864aa248a4ad040fb0acf1623cb7694339130ff0
HotelSierraWhiskey <winstontothemoon@gmail.com>
2024-12-03 20:24:10 -0500

    Updated nvic docs

--- nvic.c
+++ nvic.c
@@ -6,18 +6,24 @@
  *	D E F I N E S
  ****************************************************************************************************/
 
-/// This macro is used to alias undefined handlers as irqEMPTY_DEF until they're actually used
+/**
+ *	This macro is used to alias undefined handlers as irqEMPTY_DEF until they're actually used
+ */
 #define NVIC_WEAK_IRQ(name) extern void __attribute__((weak, alias("irqEMPTY_DEF"))) name()
 #define NVIC_VECTOR_TABLE_SIZE (155)
 
-/// IRQ function pointer
-/// 
-///	Our vector table contains 155 of these. Those that are not strongly linked will be irqEMPTY_DEF
+/**
+ *	IRQ function pointer
+ *
+ * 	Our vector table contains 155 of these. Those that are not strongly linked will be irqEMPTY_DEF
+ */
 typedef void (* NVIC_irq_function_t)();
 
-/// NVIC table struct typedef
-///
-/// Contains a pointer to the top of the stack as well as our `NVIC_irq_function_t`s
+/**
+ *	NVIC table struct typedef
+ *
+ * 	Contains a pointer to the top of the stack as well as our `NVIC_irq_function_t`s
+ */
 typedef struct _NVIC_table_t
 {
 	const void *    	kpv_stack_top;
@@ -27,101 +33,37 @@ typedef struct _NVIC_table_t
 /****************************************************************************************************
  *	F U N C T I O N   P R O T O T Y P E S
  ****************************************************************************************************/
-
-/// Weakly linked *irqSysTick* handler
 NVIC_WEAK_IRQ(irqSysTick);
-
-/// Weakly linked *irqSYSTEM* handler
 NVIC_WEAK_IRQ(irqSYSTEM);
-
-/// Weakly linked *irqWDT* handler
 NVIC_WEAK_IRQ(irqWDT);
-
-/// Weakly linked *irqRTC* handler
 NVIC_WEAK_IRQ(irqRTC);
-
-/// Weakly linked *irqEIC* handler
 NVIC_WEAK_IRQ(irqEIC);
-
-/// Weakly linked *irqFREQM* handler
 NVIC_WEAK_IRQ(irqFREQM);
-
-/// Weakly linked *irqTSENS* handler
 NVIC_WEAK_IRQ(irqTSENS);
-
-/// Weakly linked *irqNVMCTRL* handler
 NVIC_WEAK_IRQ(irqNVMCTRL);
-
-/// Weakly linked *irqDMAC* handler
 NVIC_WEAK_IRQ(irqDMAC);
-
-/// Weakly linked *irqEVSYS* handler
 NVIC_WEAK_IRQ(irqEVSYS);
-
-/// Weakly linked *irqSERCOM0* handler
 NVIC_WEAK_IRQ(irqSERCOM0);
-
-/// Weakly linked *irqSERCOM1* handler
 NVIC_WEAK_IRQ(irqSERCOM1);
-
-/// Weakly linked *irqSERCOM2* handler
 NVIC_WEAK_IRQ(irqSERCOM2);
-
-/// Weakly linked *irqSERCOM3* handler
 NVIC_WEAK_IRQ(irqSERCOM3);
-
-/// Weakly linked *irqSERCOM4* handler
 NVIC_WEAK_IRQ(irqSERCOM4);
-
-/// Weakly linked *irqSERCOM5* handler
 NVIC_WEAK_IRQ(irqSERCOM5);
-
-/// Weakly linked *irqCAN0* handler
 NVIC_WEAK_IRQ(irqCAN0);
-
-/// Weakly linked *irqCAN1* handler
 NVIC_WEAK_IRQ(irqCAN1);
-
-/// Weakly linked *irqTCC0* handler
 NVIC_WEAK_IRQ(irqTCC0);
-
-/// Weakly linked *irqTCC1* handler
 NVIC_WEAK_IRQ(irqTCC1);
-
-/// Weakly linked *irqTCC2* handler
 NVIC_WEAK_IRQ(irqTCC2);
-
-/// Weakly linked *irqTC0* handler
 NVIC_WEAK_IRQ(irqTC0);
-
-/// Weakly linked *irqTC1* handler
 NVIC_WEAK_IRQ(irqTC1);
-
-/// Weakly linked *irqTC2* handler
 NVIC_WEAK_IRQ(irqTC2);
-
-/// Weakly linked *irqTC3* handler
 NVIC_WEAK_IRQ(irqTC3);
-
-/// Weakly linked *irqTC4* handler
 NVIC_WEAK_IRQ(irqTC4);
-
-/// Weakly linked *irqADC0* handler
 NVIC_WEAK_IRQ(irqADC0);
-
-/// Weakly linked *irqADC1* handler
 NVIC_WEAK_IRQ(irqADC1);
-
-/// Weakly linked *irqAC* handler
 NVIC_WEAK_IRQ(irqAC);
-
-/// Weakly linked *irqDAC* handler
 NVIC_WEAK_IRQ(irqDAC);
-
-/// Weakly linked *irqSDADC* handler
 NVIC_WEAK_IRQ(irqSDADC);
-
-/// Weakly linked *irqPTC* handler
 NVIC_WEAK_IRQ(irqPTC);
 
 void irqRESET();
@@ -135,31 +77,23 @@ extern int main(void);
  *	V A R I A B L E S
  ****************************************************************************************************/
 
-/// *u32_stack_top* section variable (externed, provided by linkerscript)
+/**
+ *	Section variables (externed, provided by linkerscript)
+ */
 extern uint32_t u32_stack_top;
-
-/// *u32_begin_text* section variable (externed, provided by linkerscript)
 extern uint32_t u32_begin_text;
-
-/// *u32_end_text* section variable (externed, provided by linkerscript)
 extern uint32_t u32_end_text;
-
-/// *u32_begin_data* section variable (externed, provided by linkerscript)
 extern uint32_t u32_begin_data;
-
-/// *u32_end_data* section variable (externed, provided by linkerscript)
 extern uint32_t u32_end_data;
-
-/// *u32_begin_bss* section variable (externed, provided by linkerscript)
 extern uint32_t u32_begin_bss;
-
-/// *u32_end_bss* section variable (externed, provided by linkerscript)
 extern uint32_t u32_end_bss;
 
-/// The actual NVIC table, stored in the .nvic_table section of RAM
-///
-/// The vector table contains the initial stack pointer as well as the addresses of
-/// ISRs relevant to the Cortex M0+ core, as well as vendor-specific ISRs (peripherals, etc.).
+/**
+ *	The actual NVIC table, stored in the .nvic_table section of RAM
+ *
+ * 	The vector table contains the initial stack pointer as well as the addresses of
+ *	ISRs relevant to the Cortex M0+ core, as well as vendor-specific ISRs (peripherals, etc.).
+ */
 static const NVIC_table_t NVIC_table __attribute__((section(".nvic_table"), used)) =
 {
     &u32_stack_top,
@@ -217,12 +151,13 @@ static const NVIC_table_t NVIC_table __attribute__((section(".nvic_table"), used
     }
 };
 
-
 /****************************************************************************************************
  *	F U N C T I O N S
  ****************************************************************************************************/
 
-/// Minimal memset implementation
+/****************************************************************************************************
+ *	Minimal memset implementation
+ ****************************************************************************************************/
 void* memset(void* s, int c, size_t n) {
     unsigned char* p = (unsigned char*)s;
     while (n--) {
@@ -231,7 +166,9 @@ void* memset(void* s, int c, size_t n) {
     return s;
 }
 
-/// Minimal memcpy implementation
+/****************************************************************************************************
+ *	Minimal memcpy implementation
+ ****************************************************************************************************/
 void* memcpy(void* dest, const void* src, size_t n) {
     unsigned char* d = (unsigned char*)dest;
     const unsigned char* s = (const unsigned char*)src;
@@ -241,21 +178,28 @@ void* memcpy(void* dest, const void* src, size_t n) {
     return dest;
 }
 
-/// Basic "do nothing forever" irq
+/****************************************************************************************************
+ *	Basic "do nothing forever" irq
+ ****************************************************************************************************/
 void irqEMPTY_DEF()
 {
 	while(true);
 }
 
-/// Basic "do nothing forever" irq
+/****************************************************************************************************
+ *	Basic "do nothing forever" irq
+ ****************************************************************************************************/
 void irqNMI()
 {
 	while(true);
 }
 
-/// The reset handler IRQ. Called upon power up.
-/// Copies initialized source program data in flash to RAM, and then initializes the BSS section to zero.
-/// Once data is copied, main is called.
+/****************************************************************************************************
+ *	The reset handler IRQ. Called upon power up.
+ *
+ *	Copies initialized source program data in flash to RAM, and then initializes the BSS section to zero.
+ *	Once data is copied, main is called.
+ ****************************************************************************************************/
 void irqRESET()
 {
 	while(true)
@@ -272,8 +216,12 @@ void irqRESET()
 	}
 }
 
-/// Something has gone wrong. Get the process stack pointer or the main stack pointer, whichever's in use.
-/// Save the stack, trigger a breakpoint, spin in a deadloop.
+/****************************************************************************************************
+ *	Something has gone badly wrong.
+ *
+ *	Get the process stack pointer or the main stack pointer, whichever's in use.
+ *	Save the stack, trigger a breakpoint, spin in a deadloop.
+ ****************************************************************************************************/
 void irqHARD_FAULT(void)
 {
     __asm__(
