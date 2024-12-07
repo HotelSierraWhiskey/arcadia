f0cd6f921280c37502279cccff144286d09af9d7
HotelSierraWhiskey <winstontothemoon@gmail.com>
2024-12-06 21:56:44 -0500

    Increased OSC48M to full 48MHz

--- sys.c
+++ sys.c
@@ -6,8 +6,8 @@
  *	P R I V A T E   F U N C T I O N   P R O T O T Y P E S
  ****************************************************************************************************/
 
-static void 	SYS_clock_init		(void);
-static void 	SYS_osc48m_init		(void);
+static void 	SYS_clock_init			(void);
+static void 	SYS_osc48m_init			(void);
 
 /****************************************************************************************************
  *	F U N C T I O N S
@@ -31,18 +31,16 @@ void SYS_init(void)
 
 /****************************************************************************************************
  *	Initializes OSC48M (the internal 48MHz oscillator)
- *
- * 	FIXME: Why does OSCCTRL_OSC48MDIV_DIV_DIV1 cause an exception?
  * 
  ****************************************************************************************************/
 static void SYS_osc48m_init(void)
 {
-	// Apparently this is required for 48MHz, but doesn't help with the issue
-	NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS_HALF;
+	// Two wait states are required to run at 48MHz
+	NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS_DUAL;
 
 	// Enable in on-demand mode with a division factor of 2 (for 24MHz) with 21.33us startup delay
 	OSCCTRL_REGS->OSCCTRL_OSC48MCTRL = OSCCTRL_OSC48MCTRL_ENABLE(1) | OSCCTRL_OSC48MCTRL_ONDEMAND(1);
-	OSCCTRL_REGS->OSCCTRL_OSC48MDIV = OSCCTRL_OSC48MDIV_DIV_DIV2;
+	OSCCTRL_REGS->OSCCTRL_OSC48MDIV = OSCCTRL_OSC48MDIV_DIV_DIV1;
 	OSCCTRL_REGS->OSCCTRL_OSC48MSTUP = OSCCTRL_OSC48MSTUP_STARTUP_CYCLE1024;
 
 	// Wait for synchronization
@@ -64,6 +62,9 @@ static void SYS_osc48m_init(void)
  ****************************************************************************************************/
 static void SYS_clock_init(void)
 {
+	// Enable clock ready interrupt
+	MCLK_REGS->MCLK_INTENSET = MCLK_INTENSET_CKRDY(1);
+
 	// Provide GCLK0 with OSC48M as a clock source
 	GCLK_REGS->GCLK_GENCTRL[0] = GCLK_GENCTRL_SRC(GCLK_GENCTRL_SRC_OSC48M) | 
                                  GCLK_GENCTRL_GENEN(1) |
@@ -71,4 +72,13 @@ static void SYS_clock_init(void)
 								 GCLK_GENCTRL_DIV(0) |
 								 GCLK_GENCTRL_IDC(1) |
                                  GCLK_GENCTRL_OE(1);
+
+	// Division factor of one for the main clock. This is the reset/ default value
+	MCLK_REGS->MCLK_CPUDIV = MCLK_CPUDIV_CPUDIV_DIV1;
+
+	// Wait for the clock to be ready
+	while (MCLK_REGS->MCLK_INTFLAG & MCLK_INTFLAG_CKRDY(1) == 0)
+	{
+		continue;
+	}
 }
