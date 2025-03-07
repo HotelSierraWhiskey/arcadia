30a2087406c603f6097f0a743d43751faa904c90
HotelSierraWhiskey <winstontothemoon@gmail.com>
2025-03-07 16:02:25 -0500

    Documented button module

--- src/button.c
+++ src/button.c
@@ -9,6 +9,11 @@
 #define BUTTON_PRESSED		0xFFFFFFFF
 #define BUTTON_RELEASED 	0x00000000
 
+/**
+ *	A button entry
+ *
+ * 	Contains information for tracking and updating a button's state
+ */
 typedef struct _button_map
 {
 	uint32_t 			u32_history;
@@ -19,6 +24,9 @@ typedef struct _button_map
  *	P R I V A T E   V A R I A B L E S
  ****************************************************************************************************/
 
+/**
+ *	The module's registry of buttons
+ */
 static BUTTON_map_entry_t BUTTON_map[BUTTON_ID_NUM_BUTTONS] =
 {
 	[BUTTON_ID_A] =
@@ -68,6 +76,16 @@ static void 	BUTTON_read		(BUTTON_id_t button_id);
  *	F U N C T I O N S
  ****************************************************************************************************/
 
+/****************************************************************************************************
+ *	Read and update button state history
+ *
+ * 	Reads the current state of the specified button and updates its history bit-shift buffer.
+ * 	This function ensures that button presses are tracked over time for debounce handling.
+ *
+ *	@param[in] button_id The ID of the button to read
+ *
+ *	@return None
+ ****************************************************************************************************/
 static void BUTTON_read(BUTTON_id_t button_id)
 {
 	ASSERT(button_id < BUTTON_ID_NUM_BUTTONS);
@@ -82,6 +100,12 @@ static void BUTTON_read(BUTTON_id_t button_id)
 	}
 }
 
+/****************************************************************************************************
+ *	Update all button states
+ *
+ * 	Iterates through all buttons and updates their history by calling `BUTTON_read`.
+ * 
+ ****************************************************************************************************/
 void BUTTON_update_buttons(void)
 {
 	for (uint8_t u8_count = 0; u8_count < BUTTON_ID_NUM_BUTTONS; u8_count++)
@@ -90,11 +114,29 @@ void BUTTON_update_buttons(void)
 	}
 }
 
+/****************************************************************************************************
+ *	Check if a button is pressed
+ *
+ * 	Determines whether the specified button has registered as pressed based on its history buffer.
+ * 
+ *	@param[in] button_id The ID of the button to check
+ *
+ *	@return `true` if the button is pressed, `false` otherwise
+ ****************************************************************************************************/
 bool BUTTON_is_pressed(BUTTON_id_t button_id)
 {
 	return BUTTON_map[button_id].u32_history == BUTTON_PRESSED;
 }
 
+/****************************************************************************************************
+ *	Check if a button is released
+ *
+ * 	Determines whether the specified button has registered as released based on its history buffer.
+ * 
+ *	@param[in] button_id The ID of the button to check
+ *
+ *	@return `true` if the button is released, `false` otherwise
+ ****************************************************************************************************/
 bool BUTTON_is_released(BUTTON_id_t button_id)
 {
 	return BUTTON_map[button_id].u32_history == BUTTON_RELEASED;
