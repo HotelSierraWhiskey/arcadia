ee68e916a1c98000d4e50d66d1d764222adcbbad
HotelSierraWhiskey <winstontothemoon@gmail.com>
2025-03-30 13:31:53 -0400

    output VOUT

--- src/dac.c
+++ src/dac.c
@@ -1,4 +1,5 @@
 #include "dac.h"
+#include "io.h"
 
 /****************************************************************************************************
  *	F U N C T I O N S
@@ -9,6 +10,8 @@ void DAC_init(void)
 	MCLK_REGS->MCLK_APBCMASK |= MCLK_APBCMASK_DAC(1);
 
 	DAC_REGS->DAC_CTRLB = DAC_CTRLB_EOEN(1);
+
+	IO_enable_peripheral_function_for_pin(IO_PIN_ID_PA02, IO_PERIPHERAL_FUNCTION_B);
 
 	DAC_REGS->DAC_CTRLA = DAC_CTRLA_ENABLE(1);
 
