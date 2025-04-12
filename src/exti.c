c96dff9f09c5232ffe31cf7785e7d9ef2c595a87
HotelSierraWhiskey <winstontothemoon@gmail.com>
2025-04-12 01:06:32 -0400

    Decent little bit of exti work

--- src/exti.c
+++ src/exti.c
@@ -1,6 +1,16 @@
 #include "exti.h"
 #include "io.h"
+#include "utils.h"
 #include "shell.h"
+
+/****************************************************************************************************
+ *	D E F I N E S   &   T Y P E D E F S
+ ****************************************************************************************************/
+
+#define EXTI_LOG_DBG(fmt, ...)   		SHELL_printf("\r%-12s" fmt, "[EXTI]", ##__VA_ARGS__)
+#define EXTI_LOG_WARN(fmt, ...)   		SHELL_PRINT_WARNING("\r%-12s" fmt, "[EXTI]", ##__VA_ARGS__)
+
+#define EXTI_MAX_SOURCES	(16U)
 
 typedef enum _EXTI_edge_detectionid_
 {
@@ -14,59 +24,117 @@ typedef enum _EXTI_edge_detectionid_
 	EXTI_EDGE_DETECTION_NUM_IDS,
 } EXTI_edge_detection_id_t;
 
+typedef enum _EXTI_source_id
+{
+	EXTI_SOURCE_ID_DEBUG_BUTTON = 0,
+	//////////
+	EXTI_SOURCE_ID_NUM_IDS
+} EXTI_source_id_t;
+
+typedef void (* EXTI_callback_t)(void);
+
 typedef struct _EXTI_source_config_entry
 {
 	IO_pin_id_t					pin_id;
+	uint8_t						u8_extint;
 	EXTI_edge_detection_id_t	edge_detection_id;
+	EXTI_callback_t				callback;
 } EXTI_source_config_entry_t;
 
-static EXTI_source_config_entry_t EXTI_source_configs[] =
+/****************************************************************************************************
+ *	P R I V A T E   F U N C T I O N   P R O T O T Y P E S
+ ****************************************************************************************************/
+
+static void EXTI_debug_callback (void);
+
+/****************************************************************************************************
+ *	P R I V A T E   V A R I A B L E S
+ ****************************************************************************************************/
+
+/**
+ *	Registry of logical external interrupt channels
+ */
+static EXTI_source_config_entry_t EXTI_source_configs[EXTI_SOURCE_ID_NUM_IDS] =
 {
-	
+	[EXTI_SOURCE_ID_DEBUG_BUTTON] =
+	{
+		.pin_id				= IO_PIN_ID_PA09,
+		.u8_extint			= 9,
+		.edge_detection_id 	= EXTI_EDGE_DETECTION_ID_RISE,
+		.callback			= EXTI_debug_callback
+	}
 };
+
+/****************************************************************************************************
+ *	F U N C T I O N S
+ ****************************************************************************************************/
 
 void EXTI_init(void)
 {
-    // Enable APB clock for EIC
-    MCLK_REGS->MCLK_APBAMASK |= MCLK_APBAMASK_EIC_Msk;
+	ASSERT(EXTI_SOURCE_ID_NUM_IDS <= EXTI_MAX_SOURCES);
 
-    // Enable GCLK0 for EIC
-    GCLK_REGS->GCLK_PCHCTRL[EIC_GCLK_ID] = 	GCLK_PCHCTRL_CHEN(1) |
+	EXTI_source_config_entry_t * p_source_config;
+
+	// Enable APB clock for EIC
+	MCLK_REGS->MCLK_APBAMASK |= MCLK_APBAMASK_EIC_Msk;
+
+	// Enable GCLK0 for EIC
+	GCLK_REGS->GCLK_PCHCTRL[EIC_GCLK_ID] = 	GCLK_PCHCTRL_CHEN(1) |
 											GCLK_PCHCTRL_GEN_GCLK0;
 
-    // Wait until GCLK is synchronized
-    while ((GCLK_REGS->GCLK_PCHCTRL[EIC_GCLK_ID] & GCLK_PCHCTRL_CHEN(1)) == 0)
+	// Wait until GCLK is synchronized
+	while ((GCLK_REGS->GCLK_PCHCTRL[EIC_GCLK_ID] & GCLK_PCHCTRL_CHEN(1)) == 0)
 	{
 		continue;
 	}
 
-    // Configure EXTINT[9] (PA09) for rising-edge detection
-    EIC_REGS->EIC_CONFIG[1] &= ~(0x7u << 4); // Clear previous config (SENSE1 is bits [6:4])
-    EIC_REGS->EIC_CONFIG[1] |= (0x1u << 4);  // Set rising-edge detection (0x1 = RISE)
+	// Init all registered sources
+	for (uint8_t i = 0; i < EXTI_SOURCE_ID_NUM_IDS; i++)
+	{
+		p_source_config = &EXTI_source_configs[i];
 
-    // Enable interrupt for EXTINT[9]
-    EIC_REGS->EIC_INTENSET = (1 << 9);
+		// Configure edge detection. EIC has two configuration registers, each contains 8 source configs
+		if (p_source_config->u8_extint < (EXTI_MAX_SOURCES / 2))
+		{
+			EIC_REGS->EIC_CONFIG[0] |= p_source_config->edge_detection_id << p_source_config->u8_extint;
+		}
+		else
+		{
+			EIC_REGS->EIC_CONFIG[1] |= p_source_config->edge_detection_id << (p_source_config->u8_extint / 2);
+		}
 
-    // Configure PA09 pin for EXTINT[9] (Peripheral A for PA09)
-    IO_enable_peripheral_function_for_pin(IO_PIN_ID_PA09, IO_PERIPHERAL_FUNCTION_A);
+		// Enable interrupt for source
+		EIC_REGS->EIC_INTENSET |= (1 << p_source_config->u8_extint);
 
-    // Enable EIC
-    EIC_REGS->EIC_CTRLA |= EIC_CTRLA_ENABLE_Msk;
+		// Enable the peripheral function for the pin
+		IO_enable_peripheral_function_for_pin(p_source_config->pin_id, IO_PERIPHERAL_FUNCTION_A);
+	}
 
-    while (EIC_REGS->EIC_SYNCBUSY & EIC_SYNCBUSY_ENABLE_Msk)
+	// Enable EIC
+	EIC_REGS->EIC_CTRLA |= EIC_CTRLA_ENABLE_Msk;
+
+	while (EIC_REGS->EIC_SYNCBUSY & EIC_SYNCBUSY_ENABLE_Msk)
 	{
 		continue;
 	}
 
-    // Enable interrupts for EIC
-    NVIC_EnableIRQ(EIC_IRQn);
+	// Enable interrupts for EIC
+	NVIC_EnableIRQ(EIC_IRQn);
+}
+
+static void EXTI_debug_callback (void)
+{
+	EXTI_LOG_DBG("Beep!\n");
 }
 
 void irqEIC(void)
 {
-    if (EIC_REGS->EIC_INTFLAG & (1 << 9))
-    {
-        SHELL_printf("Wowie!\n");
-        EIC_REGS->EIC_INTFLAG = (1 << 9); // Clear interrupt flag for EXTINT[9]
-    }
+	for (uint8_t i = 0; i < EXTI_SOURCE_ID_NUM_IDS; i++)
+	{
+		if (EIC_REGS->EIC_INTFLAG & (1 << EXTI_source_configs[i].u8_extint))
+		{
+			EXTI_source_configs[i].callback();
+			EIC_REGS->EIC_INTFLAG |= (1 << EXTI_source_configs[i].u8_extint);
+		}
+	}
 }
