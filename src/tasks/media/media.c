8a74884d1e2cf09739d27fb9b8bdfdc331e6a2a4
HotelSierraWhiskey <winstontothemoon@gmail.com>
2025-04-13 22:21:42 -0400

    More media work and cleanup WIP

--- src/tasks/media/media.c
+++ src/tasks/media/media.c
@@ -1,3 +1,4 @@
+#include "arcadia.h"
 #include "utils.h"
 #include "dac.h"
 
@@ -7,6 +8,12 @@
 
 #define MEDIA_LOG_DBG(fmt, ...)   		SHELL_printf("\r%-12s" fmt, "[MEDIA]", ##__VA_ARGS__)
 #define MEDIA_LOG_WARN(fmt, ...)   		SHELL_PRINT_WARNING("\r%-12s" fmt, "[MEDIA]", ##__VA_ARGS__)
+
+/****************************************************************************************************
+ *	P R I V A T E   F U N C T I O N   P R O T O T Y P E S
+ ****************************************************************************************************/
+
+static void 	MEDIA_handle_message		(void);
 
 /****************************************************************************************************
  *	Pre-kernel module initialization function
@@ -29,7 +36,31 @@ void MEDIA_task(void * p_params)
 
 	while (1)
 	{
-		// MEDIA_handle_message();
+		MEDIA_handle_message();
 		vPortYield();
+	}
+}
+
+/****************************************************************************************************
+ *	MEDIA task main message handler
+ *
+ ****************************************************************************************************/
+static void MEDIA_handle_message(void)
+{
+	ARCADIA_msg_t msg;
+
+	if (ARCADIA_receive(&msg))
+	{
+		MEDIA_LOG_DBG("Received msg %s from %s\n", 
+			ARCADIA_get_msg_type(msg.id), msg.b_sent_from_isr ? "ISR" : ARCADIA_get_task_name(msg.from));
+
+		switch (msg.id)
+		{
+			case ARCADIA_MSG_ID_NOOP:
+				break;
+
+			default:
+				MEDIA_LOG_DBG("Unexpected message: %u\n", msg.id);
+		}
 	}
 }
